c02f700d261820ba5be35caaaaa9383103f5ddbc
erichon <erichon18@gmail.com>
2022-11-01 01:27:36 +0800

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,10 @@
-let temp = 0
-radio.setGroup(1)
-let diff = 5
-let prev_temp = 0
-basic.forever(function () {
-    temp = pins.analogReadPin(AnalogPin.P0)
-    if (prev_temp > temp) {
-        if (prev_temp - temp > diff) {
-            radio.sendValue("temp", temp)
-        }
-    } else {
-        if (temp - prev_temp > diff) {
-            radio.sendValue("temp", temp)
-        }
-    }
-    prev_temp = temp
+input.onButtonPressed(Button.A, function () {
+    radio.sendValue("BUT_A", 0)
 })
+input.onButtonPressed(Button.AB, function () {
+    radio.sendValue("BUT_AB", 0)
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendValue("BUT_B", 0)
+})
+radio.setGroup(1)
